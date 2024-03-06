f731bb27d2666cee0f3dd195c96d7ec3fbd03c09
Sarah T <125528922+sarahtazyan@users.noreply.github.com>
2024-03-06 13:34:41 -0800

    stopped at time field need to continue time field

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const BookingForm = () => {
-    const [date, setDates] = useState("");
-    const handleChange = (e) => {
-        setDate(e);
+const BookingForm = (props) => {
+    const [date, setDate] = useState("");
+    const [time, setTime] = useState("");
+    const [guests, setGuests] = useState("");
+    const [occasion, setOccasion] = useState(""); 
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Do something with the form data or dispatch an action
     }
+
+    const handleChange = (e) => {
+        setDate(e.target.value); // Set the value of the date state
+        // props.dispatch(e);  // Dispatch the event to update Redux
+    };
+
     return (
         <header>
             <section>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <fieldset>
                         <div>
                             <label htmlFor='book-date'>Choose Date</label>
-                            <input id='book-date' value={date} onChange={(e) => handleChange(e.target.value)} type='date' required/>
+                            <input id='book-date' value={date} onChange={handleChange} type='date' required/>
                         </div>
+                        {/* Time field */}
                     </fieldset>
                 </form>
             </section>
